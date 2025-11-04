83cc882a847e29dfb4600869e509116b81fc6c62
Luiz <161551124+Luizote1@users.noreply.github.com>
2025-11-03 21:06:35 -0300

    atualizacao de formularios

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const porta = 3100;
 
 const server = express();
 
+server.get("/", (req,res)=>{
+    req.send(`
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+        `);
+});
+
 server.listen(porta, host, ()=>{
     console.log(`Servidor Rodando em https://${host}:${porta}`)
-})
+});
